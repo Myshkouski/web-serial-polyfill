bc6bcc67703a2aff2ed29f002d61fe7cc3a18e2b
Alexei Myshkouski <am@samplify.org>
2025-06-22 10:58:26 +0300

    fix: check transferInterface_ was claimed before release

--- serial.ts
+++ serial.ts
@@ -331,7 +331,9 @@ export class SerialPort {
     this.writable_ = null;
     if (this.device_.opened) {
       await this.setSignals({dataTerminalReady: false, requestToSend: false});
-      await this.device_.releaseInterface(this.transferInterface_.interfaceNumber);
+      if (this.transferInterface_.claimed) {
+        await this.device_.releaseInterface(this.transferInterface_.interfaceNumber);
+      }
       await this.device_.close();
     }
   }
